73002d00da8d784ab4fc10f5c0ffe9764e4c76cb
DESKTOP-A5VU1EF\kim <dbgys1127@gmail.com>
2022-04-11 18:17:41 +0900

    Practice Javascript DarkMode

---
+++ Code/WEB/Practice/DarkMode.js
@@ -0,0 +1,53 @@
+var Link ={
+    setColor:function(color){
+        var alist=document.querySelectorAll('a');
+        var i=0;
+            while(i<alist.length){
+                alist[i].style.color=color;
+                i++;
+            }    
+    }
+}
+var Body ={
+    setColor:function(color){
+    var target=document.querySelector('body');  
+    target.style.color=color;
+    },
+    setBackgroundColor:function(color){
+    var target=document.querySelector('body');  
+    target.style.backgroundColor=color;
+    }
+}
+var Border={
+    setColor:function(color){
+    var border=document.getElementById('grid');
+    //var border1=document.getElementById('content');
+    var border2=document.querySelector('h1');
+    var border3=document.querySelector('ul');
+    border.style.borderBlockColor=color;
+    //border1.style.borderBottomColor=color;
+    border2.style.borderBlockColor=color;
+    border3.style.borderRightColor=color;
+    }
+}
+function dayNightHandler(self){
+    
+    if(self.value==='night')
+    {
+        Body.setColor('white');
+        Body.setBackgroundColor('black');
+        Link.setColor('powderblue');
+        Border.setColor('white');
+        self.value='day';
+
+    }else{
+
+        
+        Body.setColor('black');
+        Body.setBackgroundColor('white');
+        Link.setColor('black');
+        Border.setColor('black');
+        self.value='night';
+    
+    }
+}
