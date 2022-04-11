fb34b5790139613073a54ac6ea9cbd23fe601dd1
DESKTOP-A5VU1EF\kim <dbgys1127@gmail.com>
2022-04-11 14:18:21 +0900

    Practice Javascript add Hide and Show

---
+++ Code/WEB/Practice/Hide_Show.js
@@ -0,0 +1,31 @@
+var bDisplay=true;
+function resetDisplay(){
+    var con = document.getElementById("reset");
+    if(con.style.display!='none'){
+        con.style.display='none';
+    }
+}
+function firstDisplay(){
+    var con = document.getElementById("first");
+    if(con.style.display=='none'){
+        con.style.display='block';
+    }else{
+        con.style.display='none';
+    }
+}
+function secondDisplay(){
+    var con = document.getElementById("second");
+    if(con.style.display=='none'){
+        con.style.display='block';
+    }else{
+        con.style.display='none';
+    }
+}
+function thirdDisplay(){
+    var con = document.getElementById("third");
+    if(con.style.display=='none'){
+        con.style.display='block';
+    }else{
+        con.style.display='none';
+    }
+}
