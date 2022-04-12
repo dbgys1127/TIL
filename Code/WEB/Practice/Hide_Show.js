cf8bd745262941c5a9f61a036ffbaca8883c7995
DESKTOP-A5VU1EF\kim <dbgys1127@gmail.com>
2022-04-12 14:15:36 +0900

    Practice Javascript Add All clear

--- Code/WEB/Practice/Hide_Show.js
+++ Code/WEB/Practice/Hide_Show.js
@@ -1,4 +1,4 @@
-            var bDisplay=true;
+            /*var bDisplay=true;
             function resetDisplay(){
                 var con = document.getElementById("reset");
                 if(con.style.display!='none'){
@@ -28,4 +28,47 @@
                 }else{
                     con.style.display='none';
                 }
+            }*/
+
+            function display_on(what){
+                var target=document.getElementById('reset');
+                target.style.display='block';
+                if(what==1)
+                {
+                    var con = document.getElementById("first");
+                    if(con.style.display=='none'){
+                        con.style.display='block';
+                    }else{
+                        con.style.display='none';
+                    }
+                }
+                else if(what==2)
+                {
+                    var con = document.getElementById("second");
+                    if(con.style.display=='none'){
+                        con.style.display='block';
+                    }else{
+                        con.style.display='none';
+                    }
+                }
+                else if(what==3)
+                {
+                    var con = document.getElementById("third");
+                    if(con.style.display=='none'){
+                        con.style.display='block';
+                    }else{
+                        con.style.display='none';
+                    }
+                }   
+            }
+
+            function display_off(){
+                var con = document.getElementById("reset");
+                var con1 = document.getElementById("first");
+                var con2 = document.getElementById("second");
+                var con3 = document.getElementById("third");
+                con.style.display='none';
+                con1.style.display='none';
+                con2.style.display='none';
+                con3.style.display='none';
             }
