c2ccc4d0ac5f128dd7abcbc0ac4c0a4c446f6663
DESKTOP-A5VU1EF\kim <dbgys1127@gmail.com>
2022-06-24 10:54:50 +0900

    What computer

---
+++ Code/Java/src/Ch8_InputOutput/ExFileReader.java
@@ -0,0 +1,17 @@
+package Ch8_InputOutput;
+import java.io.*;
+public class ExFileReader {
+    public static void main(String[]args){
+        FileReader fin = null;
+        try{
+            fin=new FileReader("c:\\windows\\system.ini");
+            int c;
+            while((c=fin.read())!=-1){
+                System.out.print((char)c);
+            }
+            fin.close();
+        }catch (IOException e){
+            System.out.println("입출력 오류");
+        }
+    }
+}
