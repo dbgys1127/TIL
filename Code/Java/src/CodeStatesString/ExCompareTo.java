8fe7295ef340559753c9bbd3e4a0e6faa586984a
dbgys1127 <dbgys1127@gmail.com>
2022-07-04 13:53:21 +0900

    ADD Java JVM, 변수 vs 상수

---
+++ Code/Java/src/CodeStatesString/ExCompareTo.java
@@ -0,0 +1,75 @@
+package CodeStatesString;
+import java.util.*;
+public class ExCompareTo {
+    public static void main(String[] args){
+//        String str = new String("abcd");
+//        System.out.println("문자열 : " + str);
+//        System.out.println(str.compareTo("bcef"));
+//        System.out.println(str.compareTo("abcd") + "\n");
+//        System.out.println(str.compareTo("Abcd"));
+//        System.out.println(str.compareToIgnoreCase("Abcd"));
+//        System.out.println("compareTo() 메서드 호출 후 문자열 : " + str);
+//        String str = new String("Oracle Java");
+//        System.out.println("문자열 : " + str);
+//        System.out.println(str.indexOf('o'));
+//        System.out.println(str.indexOf('a'));
+//        System.out.println(str.indexOf("Java"));
+//        System.out.println("indexOf() 메서드 호출 후 원본 문자열 : " + str);
+//        String str = new String(" J a v a     ");
+//        System.out.println("문자열 : " + str);
+//        System.out.println(str + '|');
+//        System.out.println(str.trim() + '|');
+//        System.out.println("trim() 메서드 호출 후 문자열 : " + str);
+
+//        String str = "This is a string example using StringTokenizer";
+//        StringTokenizer tokenizer = new StringTokenizer(str);
+//        System.out.println(str);
+//        System.out.println();
+//
+//        System.out.println("total tokens:"+tokenizer.countTokens());
+//        // 아래의 while 코드는 반복문의 한 종류인데 어떻게 작동하는지 추후 반복문 챕터에서 공부할 예정이니 걱정하지 않으셔도 됩니다.
+//        while(tokenizer.hasMoreTokens()){
+//            System.out.println(tokenizer.nextToken());
+//        }
+//        System.out.println("total tokens:"+tokenizer.countTokens());
+//        StringBuilder stringBuilder = new StringBuilder();
+//        stringBuilder.append("문자열 ").append("연결");
+//        String str = stringBuilder.toString();
+//        System.out.println(stringBuilder);
+//        System.out.println(str);
+//        StringBuffer str01 = new StringBuffer();
+//        StringBuffer str02 = new StringBuffer("Java");
+//        System.out.println(str01.capacity());
+////        System.out.println(str02.capacity());
+//        StringBuffer str = new StringBuffer("Java Oracle");
+//        System.out.println("문자열 : " + str);
+//        System.out.println(str.delete(4, 8));
+//        System.out.println(str.deleteCharAt(1));
+//        System.out.println("deleteCharAt() 메소드 호출 후 문자열 : " + str);
+        StringBuffer str = new StringBuffer("Java Programming!!");
+        System.out.println("문자열 : " + str);
+        System.out.println(str.insert(4, "Script"));
+        System.out.println("insert() 메서드 호출 후 문자열 : " + str);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
