bf8c8412994779f6a1b40f95875af4b27599ceba
dbgys1127 <dbgys1127@gmail.com>
2022-07-29 16:57:33 +0900

    1

--- Code/Java_datastructure_algorithm/Ch9_Tree/test.java
+++ Code/Java_datastructure_algorithm/Ch9_Tree/test.java
@@ -1,66 +1,60 @@
 package Ch9_Tree;
 import java.util.*;
 public class test {
-    public String[] removeExtremes(String[] arr) {
-        // 수도코드
-        // 1. 공백을 입력받을 경우, null을 반환한다.
-        if(arr.length==0)
-            return null;
-        // 2. 각 문자열의 길이를 저장할 정수형 배열을 만들자. 배열의 크기는 arr 배열의 크기 만큼으로 설정
-        ArrayList<Integer> storageLen = new ArrayList<>();
+    public int removeExtremes(int[] stuff,int limit) {
+        // TODO:
+        // 문제개요
+        // 짐을 싸야하는데, 박스에는 최대 2개 짐을 넣을 수 있고
+        // 무게 제한도 있다.
+        // 박스를 최소한 사용해서 짐을 싸라
+        // stuff 배열에는 짐의 각각의 무게가
+        // limit에는 한 박스에 담을 수 있는 무게 제한이 제시된다.
+        // 위 조건에 맞게 박스를 싼 후 몇개의 박스을 썻는지 정수형 타입으로 반환 하라
 
-        // 3. storageLen에는 arr[i]의 크기를 전부 저장하자. 반복문으로 크기를 전부 저장하자
-        for(int i=0;i<arr.length;i++)
-            storageLen.add(i,arr[i].length());
+        // 주의 사항
+        //1. 짐의 개수 즉, stuff.length<=500000 다시 말해 stuff 인덱스는 최대 499999이다.
+        //2. 박스의 무게제한은 항상 짐의 무게 중 최대값보다 크지않아 쪼겔 필요는 없다.
+        //3-1-1. i  번째가 stuff의 모든 요소와 더한후 limit 보다 이하인지 파악하고 그중 가장 큰 합을 맞춘 짝꿍의 인덱스를 알아서
+        //i와 그 인덱스를 짤라야한다.
 
-        // 4. max, min 변수를 선언한다. 이때, max와 min에는 최대길이가 아닌 최대길이가 있는 위치를 저장한다.
-        int max = 0;
-        int min = 20;
-        int minIdx = 0;
-        int maxIdx = 0;
-        // 5-1. max인지 파악하기 위해 반복문을 돌리는데, arr[i].length()가 max보다 크면 바꾸는 방식으로 간다.
-        // 5-2. 이때 길이가 중복인게 있다면, 나중에 위치에 있는 문자열의 인덱스를 저장해야한다.->조건을 max<"="arr[i].lenght()하면된다.
-        for(int i=0;i<storageLen.size();i++){
-            if(max<=storageLen.get(i)){
-                max=storageLen.get(i);
-                maxIdx = storageLen.indexOf(max);
+        //수도코드
+        //1. 해당하는 짐조합이 있는지 판단하는 기준이 필요
+        //2. 배열을 ArrayList에 저장한다.->나중에 추가와 삭제를 편리하게 하기 위함이다.
+        ArrayList<Integer> loadsList = new ArrayList<>();
+        for(int i=0;i<stuff.length;i++)
+            loadsList.add(i,stuff[i]);
+        //2. 모든 요소간 합을 위해 중첩 반복문을 쓴다.
+        for(int i=0;i<loadsList.size()-1;i++){
+            //3. 요소간 합의 최대값과 그 인덱스를 저장할 변수가 필요하다.
+            int max=0;
+            int maxIdx=0;
+            boolean TF=false;
+            for(int j=i+1;j<loadsList.size();j++){
+                //4. 요소간의 합이 limit이하인지 파악한다.
+                int sumLoads = loadsList.get(i)+loadsList.get(j);
+                if(sumLoads<=limit){
+                    //5. 이때, max 보다 크면, 그 값과 인덱스를 기억한다.
+                    if(max<sumLoads){
+                        max=sumLoads;
+                        maxIdx=j;
+                        TF=true;
+                    }
+                }
+            }
+            //6. 원래, list에서 i와, j를 지우고, 맨처음에 짐의 무게범위를 넘는 집을 넣는다.
+            // 여기 두면 한개도 없을때도 빼려고 한다.
+            if(TF){
+                loadsList.remove(i);
+                loadsList.remove(maxIdx-1);
+                loadsList.add(0,250);
             }
         }
-        // 6-1. min인지 파악하기 위해 반복문을 돌리는데, arr[i].length()가 min보다 작으면 바꾸는 방식으로 간다.
-        // 6-2. 이때 길이가 중복인게 있다면, 나중에 위치에 있는 문자열의 인덱스를 저장해야한다.->조건을 min>"="arr[i].length()하면된다.
-        for(int i=0;i<storageLen.size();i++){
-            if(min>=storageLen.get(i)){
-                min=storageLen.get(i);
-                minIdx = storageLen.indexOf(min);
-            }
-        }
-
-        // 7. 이제 인자로 받은 배열의 중간을 삭제, 추가 할수있는 ArrayList를 만들어서 여기에 arr[i]들을 저장하자.
-        ArrayList<String> preresult = new ArrayList<>(Arrays.asList(arr));
-
-        // 8-1. min과 max 인덴스 값을 제거한다.
-        // 8-2. 근데 이때 주의해야 할 것은 무언가 하나 제거하면, 제거한 자리뒤에 있는 것들이 한칸씩 앞으로 댕긴다는 것이다.
-        // 8-3. 그럼 min과 max중 누가 앞에 있는지를 따져야 한다. 만약 min<max 이라면, max-1의 값을 제거 해야하고, 반대도 마찬가지다.
-        // 8-4. 따라서 if문으로 먼저 시작한다.
-        if(minIdx<maxIdx){
-            preresult.remove(minIdx);
-            preresult.remove(maxIdx-1);
-        }else {
-            preresult.remove(maxIdx);
-            preresult.remove(minIdx-1);
-        }
-        // 9. 끝으로 ArrayList를 다시 배열로 만들어야한다.
-        String [] result = new String[preresult.size()];
-        for(int i =0; i<result.length;i++) {
-            result[i] = preresult.get(i);
-            System.out.println(result[i]);
-        }
-        return result;
+        return loadsList.size();
     }
 
     public static void main(String[] args) {
         test t = new test();
-        String[] output = t.removeExtremes(new String[]{"a", "c", "def"});
+        int output = t.removeExtremes(new int[]{42, 25, 60, 73, 103, 167}, 187);
         System.out.println(output); // --> ["a"', "b"]
     }
 }
---
+++ Code/Java_datastructure_algorithm/Ch9_Tree/test1.java
@@ -0,0 +1,12 @@
+package Ch9_Tree;
+import java.util.*;
+public class test1 {
+    public static void main(String[] args) {
+
+        String direction="adbdgdfdf";
+        String [] dirArray = direction.split("");
+        for(int i=0;i<dirArray.length;i++){
+            System.out.println(dirArray[i]);
+        }
+    }
+}
