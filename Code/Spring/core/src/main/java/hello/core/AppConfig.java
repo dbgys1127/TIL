fba72f549871d20bc74e3d1f1afdbf2ecb01b221
dbgys1127 <dbgys1127@gmail.com>
2022-08-11 16:11:13 +0900

    스프링 컨테이너 적용

--- Code/Spring/core/src/main/java/hello/core/AppConfig.java
+++ Code/Spring/core/src/main/java/hello/core/AppConfig.java
@@ -9,20 +9,28 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
+    @Bean
     public MemberService memberService(){
         // 생성자 주입
         return new MemberServiceImpl(memberRepository());
     }
 
-    private static MemberRepository memberRepository() {
+    @Bean
+    public static MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService(){
         return new OrderServiceImpl(discountPolicy(), memberRepository());
     }
+
+    @Bean
     public DiscountPolicy discountPolicy(){
         //return new FixDiscountPolicy();
         //정률 할인으로 변경
