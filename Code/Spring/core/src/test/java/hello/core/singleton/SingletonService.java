6399571930cc5da0c49742690a113088bb5670d9
dbgys1127 <dbgys1127@gmail.com>
2022-08-11 23:52:55 +0900

    스프링컨테이너의 싱글톤 지원

--- Code/Spring/core/src/test/java/hello/core/singleton/SingletonService.java
+++ Code/Spring/core/src/test/java/hello/core/singleton/SingletonService.java
@@ -1,5 +1,15 @@
 package hello.core.singleton;
 
 public class SingletonService {
-    private static 
+    private static final SingletonService instance = new SingletonService();
+
+    public static SingletonService getInstance(){
+        return instance;
+    }
+    private SingletonService(){
+
+    }
+    public void logic(){
+        System.out.println("싱글톤 객체 로직 호출");
+    }
 }
