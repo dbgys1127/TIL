572e23449049efc373a2bae1635293a8c5834931
dbgys1127 <dbgys1127@gmail.com>
2022-08-11 15:55:52 +0900

    의존성 주입

--- Code/Spring/core/src/main/java/hello/core/AppConfig.java
+++ Code/Spring/core/src/main/java/hello/core/AppConfig.java
@@ -2,6 +2,7 @@ package hello.core;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
@@ -23,6 +24,8 @@ public class AppConfig {
         return new OrderServiceImpl(discountPolicy(), memberRepository());
     }
     public DiscountPolicy discountPolicy(){
-        return new FixDiscountPolicy();
+        //return new FixDiscountPolicy();
+        //정률 할인으로 변경
+        return new RateDiscountPolicy();
     }
 }
