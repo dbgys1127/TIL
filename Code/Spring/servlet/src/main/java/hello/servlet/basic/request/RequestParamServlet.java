c8dbbfc87591bdee4bc8a9c938e323f9161b15cc
dbgys1127 <dbgys1127@gmail.com>
2022-08-26 21:39:39 +0900

    서블릿 학습

---
+++ Code/Spring/servlet/src/main/java/hello/servlet/basic/request/RequestParamServlet.java
@@ -0,0 +1,38 @@
+package hello.servlet.basic.request;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * 1. 파라미터 전송 기능
+ */
+@WebServlet(name = "requestParamServlet",urlPatterns = "/request-param")
+public class RequestParamServlet extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String method = request.getMethod();
+        System.out.println("method = " + method);
+        System.out.println("[전체 파라미터 조회] - start");
+        request.getParameterNames().asIterator()
+                        .forEachRemaining(paramName -> System.out.println(paramName+"="+request.getParameter(paramName)));
+        System.out.println("[전체 파라미터 조회] - end");
+        System.out.println();
+        System.out.println("[단일 파라미터 조회]");
+        String username = request.getParameter("username");
+        System.out.println("username = " + username);
+        String age = request.getParameter("age");
+        System.out.println("age = " + age);
+        System.out.println("[단일 파라미터 조회 끝]");
+
+        System.out.println("[이름이 같은 복수 파라미터 조회]");
+        String[] usernames = request.getParameterValues("username");
+        for(String name:usernames){
+            System.out.println("username : "+name);
+        }
+        response.getWriter().write("ok");
+    }
+}
