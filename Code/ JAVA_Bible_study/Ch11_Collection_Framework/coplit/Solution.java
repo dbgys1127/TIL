74ae5cc0c0fb215528fbfa67d97d79cea276d031
dbgys1127 <dbgys1127@gmail.com>
2022-09-08 13:48:07 +0900

    mvc로 서비스 만들기 실습

--- Code/ JAVA_Bible_study/Ch11_Collection_Framework/coplit/Solution.java
+++ Code/ JAVA_Bible_study/Ch11_Collection_Framework/coplit/Solution.java
@@ -3,59 +3,73 @@ package Ch11_Collection_Framework.coplit;
 import java.util.*;
 
 public class Solution {
-    public int robotPath(int[][] room, int[] src, int[] dst) {
+    public int[][] rotateMatrix(int[][] matrix, int K) {
+        // TODO:
         // 문제개요
-        // 장애물이 있는 room에서 최소경로를 찾아서 src에서 dst로 도착해야한다.
-        // 주의 해야할 것
-        // 왔던길을 어떻게 하면 다시 안가는가?
-        // 장애물을 어떻게 피할 것인가?
-        // 최소경로로 어떻게 갈 것인가?
-        // 최소길이를 어떻게 기록할 것인가?
-        // 위 네가지를 주의해서 풀어야한다.
+        // 정사각형 이차원 배열을 1회마다 90도씩 시계방향으로 꺽는 문제다.
+        // 근데 여기서, 90씩 일정한 방향이면,
+        // 4의 나머지에 따라 배열을 그룹화 할 수 있게된다.
 
         // 수도 코드
-        // 1. 재귀함수를 호출한다.
-        // 이때, '1'은 로봇의 출발위치에 다시 오지 않기 위해서 체크하는 것이다.
-        room = tMap(room.length,room[0].length,1,src,room);
-
-        // 2. 결과는 로봇이 room의 목적지에 왔을때, 이동횟수를 반환하면 된다.
-        // 이때, -1을 하는 이유는 로봇이 출발위치에서 이동하지 않았는데, 다시오지 않게 하기위해
-        // 1로 기록을 해놨기 때문이다.
-        return room[dst[0]][dst[1]]-1;
+        if(matrix == null || matrix.length == 0) return matrix;
+        // 1. K를 4로 나눈 나머지를 저장하는 변수를 만든다.
+        int group = K%4;
+        // 2. 행열을 구해주는 함수로 보낸후 거기서 받은 값으로 제출한다.
+        return rotateRectangle(matrix,group);
     }
-    public int[][] tMap(int M, int N, int time, int[] src, int[][] room){
-        // 3. 현재 room에서 로봇위치를 나타낼것이다.
-        int row = src[0];
-        int col = src[1];
+    public int[][] rotateRectangle(int[][]matrix,int group){
+        int[][] resultMatrix = new int [matrix.length][matrix[0].length];
 
-        // 4. 로봇이 room을 이탈할 경우 아무 작업도 하지 않게 그 레벨 재귀를 끝낸다.
-        if(row<0||row>M-1||col<0||col>N-1){
-            return room;
+        switch(group){
+            case 0: return matrix;
+            case 1:
+                for(int i=0;i<matrix.length;i++){
+                    int k = matrix[0].length-1;
+                    for(int j=0;j<matrix[0].length;j++){
+                        resultMatrix[i][j]=matrix[k][i];
+                        k--;
+                    }
+                }
+                break;
+            case 2:
+                int l = matrix.length-1;
+                for(int i=0;i<matrix.length;i++){
+                    int k = matrix[0].length-1;
+                    for(int j=0;j<matrix[0].length;j++){
+                        resultMatrix[i][j]=matrix[l][k];
+                        k--;
+                    }
+                    l--;
+                }
+                break;
+            case 3:
+                int q = matrix.length-1;
+                for(int i=0;i<matrix.length;i++){
+                    for(int j=0;j<matrix[0].length;j++){
+                        resultMatrix[i][j]=matrix[j][q];
+                    }
+                    q--;
+                }
+                break;
         }
-
-        // 5. 로봇이 재방문하거나 최소경로인 곳을 가게 되면, 현재 step으로 바꾼다.
-        // 여기서 최소경로인 곳이라는 것은, 목표 지점에 왔을때, 다른 재귀를 통해 온
-        // 시간이 10이고, 최소경로가 8이면, 8로 변경되어야함을 의미한다.
-        if(room[row][col]==0||room[row][col]>step){
-            room[row][col]=step;
-        }else{
-            return room;
-        }
-
-        tMap(M,N,new int[]{row-1,col},step+1,room);//상
-        tMap(M,N,new int[]{row+1,col},step+1,room);//상
-        tMap(M,N,new int[]{row,col-1},step+1,room);//좌
-        tMap(M,N,new int[]{row,col+1},step+1,room);//우
-
-        return room;
+        return resultMatrix;
     }
     public static void main(String[] args) {
         Solution test = new Solution();
-        // ["", "j", "jm", "jmp", "jmpu", "jmu", "jp", "jpu", "ju", "m", "mp", "mpu", "mu", "p", "pu", "u"]
-        System.out.println(test.powerSet("jump"));
-        System.out.println(test.powerSet("bca"));
-        System.out.println(test.powerSet("7491"));
-        System.out.println(test.powerSet("lion"));
+        int[][] matrix = new int[][]{
+                {1, 2, 3, 4},
+                {5, 6, 7, 8},
+                {9, 10, 11, 12},
+                {13, 14, 15, 16}
+        };
+        int K =121;
+        int[][] result = test.rotateMatrix(matrix, K);
+        for(int i=0;i<result.length;i++){
+            for (int j = 0; j < result[0].length; j++) {
+                System.out.print(result[i][j]+" ");
+            }
+            System.out.println();
+        }
 
     }
 }
--- Code/Spring/item-service/src/main/java/hello/itemservice/web/item/basic/BasicItemController.java
+++ Code/Spring/item-service/src/main/java/hello/itemservice/web/item/basic/BasicItemController.java
@@ -2,13 +2,12 @@ package hello.itemservice.web.item.basic;
 
 import hello.itemservice.domain.item.Item;
 import hello.itemservice.domain.item.ItemRepository;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
@@ -25,6 +24,90 @@ public class BasicItemController {
         model.addAttribute("items", items);
         return "basic/items";
     }
+    @GetMapping("/{itemId}")
+    public String item(@PathVariable long itemId, Model model){
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "basic/item";
+    }
+    @GetMapping("/add")
+    public String addForm() {
+        return "basic/addForm";
+    }
+    @GetMapping("/{itemId}/edit")
+    public String editForm(@PathVariable Long itemId,Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "basic/editForm";
+    }
+    @PostMapping("/{itemId}/edit")
+    public String edit(@PathVariable Long itemId,@ModelAttribute Item item) {
+        itemRepository.update(itemId,item);
+        return "redirect:/basic/items/{itemId}";
+    }
+    @PostMapping("/add")
+    public String addItemV6(Item item, RedirectAttributes redirectAttributes) {
+        Item savedItem = itemRepository.save(item);
+
+        redirectAttributes.addAttribute("itemId",savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+
+        return "redirect:/basic/items/{itemId}";
+    }
+//    @PostMapping("/add")
+//    public String addItemV5(Item item) {
+//
+//        itemRepository.save(item);
+//
+//        return "redirect:/basic/items/"+item.getId();
+//    }
+//    @PostMapping("/add")
+//    public String addItemV4(Item item) {
+//
+//        //Item -> item (파라미터의 객체를 이렇게 변환해서 Attribute에 전달됨)
+//        itemRepository.save(item);
+//
+//        //model.addAttribute("item", item);
+//
+//        return "basic/item";
+//    }
+//    @PostMapping("/add")
+//    public String addItemV3(@ModelAttribute Item item,Model model) {
+//
+//        //Item -> item (파라미터의 객체를 이렇게 변환해서 Attribute에 전달됨)
+//        itemRepository.save(item);
+//
+//        //model.addAttribute("item", item);
+//
+//        return "basic/item";
+//    }
+//    @PostMapping("/add")
+//    public String addItemV2(@ModelAttribute("item")Item item,Model model) {
+//
+//        //@ModelAttribute는 객체를 만들고 set까지 해주는데 "item"하면 addAttribute까지 해준다.
+//        itemRepository.save(item);
+//
+//        //model.addAttribute("item", item);
+//
+//        return "basic/item";
+//    }
+//    @PostMapping("/add")
+//    public String addItemV1(@RequestParam String itemName,
+//                       @RequestParam int price,
+//                       @RequestParam Integer quantity,
+//                       Model model) {
+//        Item item = new Item();
+//        item.setItemName(itemName);
+//        item.setPrice(price);
+//        item.setQuantity(quantity);
+//
+//        itemRepository.save(item);
+//
+//        model.addAttribute("item", item);
+//
+//        return "basic/item";
+//    }
+
 
     @PostConstruct
     public void init() {
