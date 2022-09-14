f8cd9b859fcda9c9087ed77f8dac77dd681c3de3
dbgys1127 <dbgys1127@gmail.com>
2022-09-15 01:03:05 +0900

    Validation 학습

---
+++ Code/Spring/validation/src/test/java/hello/itemservice/validation/MessageCodesResolverTest.java
@@ -0,0 +1,39 @@
+package hello.itemservice.validation;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.validation.DefaultMessageCodesResolver;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.MessageCodesResolver;
+import org.springframework.validation.ObjectError;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class MessageCodesResolverTest {
+    MessageCodesResolver codesResolver = new DefaultMessageCodesResolver();
+
+    @Test
+    void messageCodesResolverObject() {
+        String[] messageCodes = codesResolver.resolveMessageCodes("required", "item");
+        for (String messageCode : messageCodes) {
+            System.out.println("messageCode = " + messageCode);
+        }
+        assertThat(messageCodes).containsExactly("required.item", "required");
+    }
+    @Test
+    void messageCodesResolverField() {
+        String[] messageCodes = codesResolver.resolveMessageCodes("required", "item", "itemName", String.class);
+        for (String messageCode : messageCodes) {
+            System.out.println("messageCode = " + messageCode);
+        }
+        assertThat(messageCodes).containsExactly("required.item.itemName", "required.itemName","required.java.lang.String","required");
+        // bindingResult.rejectValue("itemName","required");
+        //new FieldError("item","itemName",null,false, messageCodes,null,null);
+        /**
+         * 1순위 코드.객체.필드
+         * 2순위 코드.(객체생략)필드
+         * 3순위 코드.필드타입
+         * 4순위 코드
+         */
+    }
+}
