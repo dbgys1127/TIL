926f317298725a25197d11851a46853afb23d79b
dbgys1127 <dbgys1127@gmail.com>
2022-09-18 20:02:00 +0900

    예외처리 및 필터와 인터셉터

--- Code/Spring/login/src/main/java/hello/login/web/HomeController.java
+++ Code/Spring/login/src/main/java/hello/login/web/HomeController.java
@@ -2,7 +2,9 @@ package hello.login.web;
 
 import hello.login.domain.member.Member;
 import hello.login.domain.member.MemberRepository;
+import hello.login.web.argumentresolver.Login;
 import hello.login.web.login.LoginForm;
+import hello.login.web.session.SessionManager;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
@@ -10,9 +12,12 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
 import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 @Slf4j
 @Controller
@@ -20,14 +25,15 @@ import javax.servlet.http.HttpServletResponse;
 public class HomeController {
 
     private final MemberRepository memberRepository;
+    private final SessionManager sessionManager;
 
    // @GetMapping("/")
     public String home() {
         return "home";
     }
 
-    @GetMapping("/")
-    public String homeLogin(@CookieValue(name = "memberId",required = false)Long memberId, Model model) {
+    //@GetMapping("/")
+    public String homeLoginV1(@CookieValue(name = "memberId",required = false)Long memberId, Model model) {
         if (memberId == null) {
             return "home";
         }
@@ -41,16 +47,60 @@ public class HomeController {
         model.addAttribute("member", loginMember);
         return "loginHome";
     }
+    //@GetMapping("/")
+    public String homeLoginV2(HttpServletRequest request, Model model) {
 
-    @PostMapping("/logout")
-    public String logout(HttpServletResponse response) {
-        expireCookie(response, "memberId");
-        return "redirect:/";
+        //세션 관리자에 저장된 회원 정보 조회
+        Member member = (Member) sessionManager.getSession(request);
+
+
+        //로그인
+        if (member == null) {
+            return "home";
+        }
+
+        model.addAttribute("member", member);
+        return "loginHome";
     }
+    //@GetMapping("/")
+    public String homeLoginV3(HttpServletRequest request, Model model) {
 
-    private static void expireCookie(HttpServletResponse response, String cookieName) {
-        Cookie cookie = new Cookie(cookieName, null);
-        cookie.setMaxAge(0);
-        response.addCookie(cookie);
+        //세션 관리자에 저장된 회원 정보 조회
+        HttpSession session = request.getSession(false);
+        if (session == null) {
+            return "home";
+        }
+        Member loginMember = (Member) session.getAttribute(SessionConst.LOGIN_MEMBER);
+
+        //로그인
+        if (loginMember == null) {
+            return "home";
+        }
+
+        model.addAttribute("member", loginMember);
+        return "loginHome";
+    }
+    //@GetMapping("/")
+    public String homeLoginV3Spring(
+            @SessionAttribute(name=SessionConst.LOGIN_MEMBER,required = false) Member loginMember, Model model) {
+
+        //세션 관리자에 저장된 회원 정보 조회
+        if (loginMember == null) {
+            return "home";
+        }
+
+        model.addAttribute("member", loginMember);
+        return "loginHome";
+    }
+    @GetMapping("/")
+    public String homeLoginV3ArgumentResolver(@Login Member loginMember, Model model) {
+
+        //세션 관리자에 저장된 회원 정보 조회
+        if (loginMember == null) {
+            return "home";
+        }
+
+        model.addAttribute("member", loginMember);
+        return "loginHome";
     }
 }
--- Code/Spring/login/src/test/java/hello/login/web/session/SessionManagerTest.java
+++ Code/Spring/login/src/test/java/hello/login/web/session/SessionManagerTest.java
@@ -1,4 +1,39 @@
 package hello.login.web.session;
 
+import hello.login.domain.member.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+
+import javax.servlet.http.HttpServletResponse;
+
+import static org.assertj.core.api.Assertions.*;
+
 public class SessionManagerTest {
+
+    SessionManager sessionManager = new SessionManager();
+
+    @Test
+    void sessionTest() {
+        // 세션 생성
+        MockHttpServletResponse response = new MockHttpServletResponse();
+
+        Member member = new Member();
+        sessionManager.createSession(member, response);
+
+        //웹브라우저(클라이언트) 요청에 응답 쿠키 저장
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        request.setCookies(response.getCookies()); // mysessionid = ssss-1-ereee-eff
+
+        //세션조회
+        Object result = sessionManager.getSession(request);
+        assertThat(result).isEqualTo(member);
+
+        //세션 만료
+        sessionManager.expire(request);
+
+        Object expired=sessionManager.getSession(request);
+        assertThat(expired).isEqualTo(null);
+    }
 }
