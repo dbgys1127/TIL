3f3f63167d21b44da67ac4f20caf82af87c4ef64
dbgys1127 <dbgys1127@gmail.com>
2022-09-20 17:16:25 +0900

    파일 업로드 실습

---
+++ Code/Spring/codestates/httpstestproject/src/main/java/com/codestates/httpstestproject/HttpstestprojectApplication.java
@@ -0,0 +1,13 @@
+package com.codestates.httpstestproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttpstestprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HttpstestprojectApplication.class, args);
+	}
+
+}
---
+++ Code/Spring/codestates/httpstestproject/src/test/java/com/codestates/httpstestproject/HttpstestprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codestates.httpstestproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HttpstestprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
