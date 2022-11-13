e6f02d1d6c1c6baca98fbd014a125dafc94c32fa
dbgys1127 <dbgys1127@gmail.com>
2022-11-13 23:48:32 +0900

    OAuth2

--- Code/Spring/jwt/src/main/java/com/cos/jwt/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.cos.jwt.repository;
-
-import com.cos.jwt.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User,Long> {
-    User findByUsername(String username);//Jpa Query method
-}
